ee0493c748868a926ed4588e318832319e8652a8
Andrei Preda <andreipreda07@gmail.com>
2017-03-08 22:20:39 +0200

    Consolidate parsers into generic functions

--- Data/Attoparsec/Binary.hs
+++ Data/Attoparsec/Binary.hs
@@ -31,29 +31,35 @@ anyWordN = anyWordN' undefined
   where anyWordN' :: (FiniteBits a) => a -> (B.ByteString -> a) -> Parser a
         anyWordN' d = flip fmap $ Data.Attoparsec.ByteString.take $ byteSize d
 
+anyWordNbe :: (FiniteBits a, Num a) => Parser a
+anyWordNbe = anyWordN pack
+
+anyWordNle :: (FiniteBits a, Num a) => Parser a
+anyWordNle = anyWordN $ pack . B.reverse
+
 -- |Match any 16-bit big-endian word.
 anyWord16be :: Parser Word16
-anyWord16be = anyWordN pack
+anyWord16be = anyWordNbe
 
 -- |Match any 16-bit little-endian word.
 anyWord16le :: Parser Word16
-anyWord16le = anyWordN $ pack . B.reverse
+anyWord16le = anyWordNle
 
 -- |Match any 32-bit big-endian word.
 anyWord32be :: Parser Word32
-anyWord32be = anyWordN pack
+anyWord32be = anyWordNbe
 
 -- |Match any 32-bit little-endian word.
 anyWord32le :: Parser Word32
-anyWord32le = anyWordN $ pack . B.reverse
+anyWord32le = anyWordNle
 
 -- |Match any 64-bit big-endian word.
 anyWord64be :: Parser Word64
-anyWord64be = anyWordN pack
+anyWord64be = anyWordNbe
 
 -- |Match any 64-bit little-endian word.
 anyWord64le :: Parser Word64
-anyWord64le = anyWordN $ pack . B.reverse
+anyWord64le = anyWordNle
 
 unpack :: (FiniteBits a, Integral a) => a -> B.ByteString
 unpack x = B.pack $ map f $ reverse [0..byteSize x - 1]
@@ -62,26 +68,32 @@ unpack x = B.pack $ map f $ reverse [0..byteSize x - 1]
 wordN :: (a -> B.ByteString) -> a -> Parser a
 wordN u w = string (u w) >> return w
 
+wordNbe :: (FiniteBits a, Integral a) => a -> Parser a
+wordNbe = wordN unpack
+
+wordNle :: (FiniteBits a, Integral a) => a -> Parser a
+wordNle = wordN $ B.reverse . unpack
+
 -- |Match a specific 16-bit big-endian word.
 word16be :: Word16 -> Parser Word16
-word16be = wordN unpack
+word16be = wordNbe
 
 -- |Match a specific 16-bit little-endian word.
 word16le :: Word16 -> Parser Word16
-word16le = wordN $ B.reverse . unpack
+word16le = wordNle
 
 -- |Match a specific 32-bit big-endian word.
 word32be :: Word32 -> Parser Word32
-word32be = wordN unpack
+word32be = wordNbe
 
 -- |Match a specific 32-bit little-endian word.
 word32le :: Word32 -> Parser Word32
-word32le = wordN $ B.reverse . unpack
+word32le = wordNle
 
 -- |Match a specific 64-bit big-endian word.
 word64be :: Word64 -> Parser Word64
-word64be = wordN unpack
+word64be = wordNbe
 
 -- |Match a specific 64-bit little-endian word.
 word64le :: Word64 -> Parser Word64
-word64le = wordN $ B.reverse . unpack
+word64le = wordNle
